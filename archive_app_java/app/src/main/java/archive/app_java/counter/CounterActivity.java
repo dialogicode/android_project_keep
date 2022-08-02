063fdda602f5416e58ed2ff14d3c2057240e22c7
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-08-02 11:53:11 +0900

    java > counter > +viewBinding -findViewById()

--- archive_app_java/app/src/main/java/archive/app_java/counter/CounterActivity.java
+++ archive_app_java/app/src/main/java/archive/app_java/counter/CounterActivity.java
@@ -2,11 +2,9 @@ package archive.app_java.counter;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import archive.app_java.BaseActivity;
-import archive.app_java.R;
+import archive.app_java.databinding.ActivityCounterBinding;
 
 public class CounterActivity extends BaseActivity {
 	private int count = 0;
@@ -14,25 +12,24 @@ public class CounterActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_counter);
 
-		TextView count_show = findViewById(R.id.count_show);
-		Button count_plus = findViewById(R.id.count_plus);
-		Button count_minus = findViewById(R.id.count_minus);
+		// set viewBinding & layout
+		ActivityCounterBinding bind = ActivityCounterBinding.inflate(getLayoutInflater());
+		setContentView(bind.getRoot());
 
-		count_show.setText(Integer.toString(count));
+		bind.countShow.setText(Integer.toString(count));
 
-		count_plus.setOnClickListener(new View.OnClickListener() {
+		bind.countPlus.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				count++;
-				count_show.setText(Integer.toString(count));
+				bind.countShow.setText(Integer.toString(count));
 			}
 		});
 
-		count_minus.setOnClickListener(v -> {
+		bind.countMinus.setOnClickListener(v -> {
 			count--;
-			count_show.setText(Integer.toString(count));
+			bind.countShow.setText(Integer.toString(count));
 		});
 	}
 }
