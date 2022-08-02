32f63e718484762d6f1f5fac441a108ebf03ecb2
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-08-02 12:51:44 +0900

    java > counter > +ViewModel -data

--- archive_app_java/app/src/main/java/archive/app_java/counter/CounterActivity.java
+++ archive_app_java/app/src/main/java/archive/app_java/counter/CounterActivity.java
@@ -3,33 +3,39 @@ package archive.app_java.counter;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.lifecycle.ViewModelProvider;
+
 import archive.app_java.BaseActivity;
 import archive.app_java.databinding.ActivityCounterBinding;
 
 public class CounterActivity extends BaseActivity {
-	private int count = 0;
+	private CounterViewModel vm;
+	private ActivityCounterBinding bind;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		// set ViewModel
+		vm = new ViewModelProvider(this).get(CounterViewModel.class);
+
 		// set viewBinding & layout
-		ActivityCounterBinding bind = ActivityCounterBinding.inflate(getLayoutInflater());
+		bind = ActivityCounterBinding.inflate(getLayoutInflater());
 		setContentView(bind.getRoot());
 
-		bind.countShow.setText(Integer.toString(count));
+		bind.countShow.setText(Integer.toString(vm.count));
+		
+		bind.countPlus.setOnClickListener(this::click_listener_plus);
+		bind.countMinus.setOnClickListener(this::click_listener_minus);
+	}
 
-		bind.countPlus.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				count++;
-				bind.countShow.setText(Integer.toString(count));
-			}
-		});
+	private void click_listener_plus(View view) {
+		vm.count++;
+		bind.countShow.setText(Integer.toString(vm.count));
+	}
 
-		bind.countMinus.setOnClickListener(v -> {
-			count--;
-			bind.countShow.setText(Integer.toString(count));
-		});
+	private void click_listener_minus(View view) {
+		vm.count--;
+		bind.countShow.setText(Integer.toString(vm.count));
 	}
 }
---
+++ archive_app_java/app/src/main/java/archive/app_java/counter/CounterViewModel.java
@@ -0,0 +1,11 @@
+package archive.app_java.counter;
+
+import archive.app_java.BaseViewModel;
+
+public class CounterViewModel extends BaseViewModel {
+	public int count;
+
+	public CounterViewModel() {
+		this.count = 0;
+	}
+}
